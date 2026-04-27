1fd6ef0170e480b7514b0a1ac93c779ae0176168
xychen1104 <xychen1104@gmail.com>
2026-04-27 09:22:47 +0800

    Update run_retraining_docker.sh

--- training/run_retraining_docker.sh
+++ training/run_retraining_docker.sh
@@ -11,7 +11,7 @@ sudo docker run --rm \
   -e DATA_API_BASE="http://129.114.27.10:30800" \
   -e RETRAIN_DATA_DIR="/app/training/data" \
   -e MIN_RETRAIN_EXAMPLES="1" \
-  -e MEETING_IDS="cb0f7be0-e418-4258-880f-0048c52b3c59,369f286d-a70f-48e1-9f7d-fe5487b6e727,d7b68cba-c06d-484e-8046-226c1511d654,dc137b0c-374f-4bdb-a6c7-0ab7b54bbdfb,5325d089-528e-4777-9bf3-f0ae93c42f10,769ba28f-da29-4f65-8267-5398a6b4821f" \
+  -e MEETING_ID_SOURCE="api" \
   -v "$PWD/data:/app/training/data" \
   -v "$PWD/outputs:/app/training/outputs" \
   proj27-training \
